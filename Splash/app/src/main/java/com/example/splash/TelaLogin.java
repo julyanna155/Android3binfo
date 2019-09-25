9942c5f7b26e462da10f986fb73a316580d77ee9
Rubens Leme <rubenss.leme@gmail.com>
2019-09-24 21:55:13 -0300

    Tela de Login

--- Splash/app/src/main/java/com/example/splash/TelaLogin.java
+++ Splash/app/src/main/java/com/example/splash/TelaLogin.java
@@ -2,13 +2,43 @@ package com.example.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TelaLogin extends AppCompatActivity {
+
+    Button btnEnviar; // Declaração de Um Button
+    Button btnCancelar; // Declaração de Um Button
+    EditText edtUsuario;
+    EditText edtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+        inicializarObjeto();
+        abrirActivityMenu();
     }
+
+    public void inicializarObjeto() {
+        btnEnviar = (Button) findViewById(R.id.btnEnviar); // Atribuição de valor
+        btnCancelar = (Button) findViewById(R.id.btnCancelar);
+        edtUsuario = (EditText) findViewById(R.id.edtUsuario);
+        edtSenha = (EditText) findViewById(R.id.edtSenha);
+    }
+
+
+    private void abrirActivityMenu() {
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentActivityMenu = new Intent(getApplicationContext(), TelaMenu.class);
+                startActivity(intentActivityMenu);
+            }
+        });
+    }
+
 }
